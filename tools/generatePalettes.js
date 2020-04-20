c2c014fc99c4a40a86cfb7462ab9ec8115849122
Ioannis Kirmitzoglou <ioanniskirmitzoglou@gmail.com>
2020-04-20 09:10:19 +0100

    Better palette

--- tools/generatePalettes.js
+++ tools/generatePalettes.js
@@ -1,7 +1,7 @@
 var ColorScheme = require('color-scheme');
 
 var scheme = new ColorScheme
-var variations = ['soft', 'pastel'];
+var variations = ['default', 'soft', 'pastel', 'light', 'hard'];
 console.log("[");
 for (let i = 0; i < 360; i++) {
     for (let j = 0; j < variations.length; j++) {
@@ -11,10 +11,11 @@ for (let i = 0; i < 360; i++) {
         .scheme('contrast')     
         .variation(variation);   // Use the 'soft' color variation
         var colors = scheme.colors();
-        console.log('["#' + colors[0] + '", "#' + colors[7] + '"],');
-        console.log('["#' + colors[1] + '", "#' + colors[6] + '"],');
-        console.log('["#' + colors[2] + '", "#' + colors[5] + '"],');
-        console.log('["#' + colors[3] + '", "#' + colors[4] + '"],');
+        console.log('["#' + colors[0] + '", "#' + colors[4] + '"],');
+        // console.log('["#' + colors[0] + '", "#' + colors[7] + '"],');
+        // console.log('["#' + colors[1] + '", "#' + colors[6] + '"],');
+        // console.log('["#' + colors[2] + '", "#' + colors[5] + '"],');
+        // console.log('["#' + colors[3] + '", "#' + colors[4] + '"],');
 
     }
 }
